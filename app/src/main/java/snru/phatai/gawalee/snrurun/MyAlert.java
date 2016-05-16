d65cc43ba37388b52fe38bb3707839ddb446bc5c
gawalee <g.gawalee@gmail.com>
2016-05-16 15:41:17 +0700

    AlertDialog

---
+++ app/src/main/java/snru/phatai/gawalee/snrurun/MyAlert.java
@@ -0,0 +1,17 @@
+package snru.phatai.gawalee.snrurun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+/**
+ * Created by gawalee on 5/16/16 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    }
+
+} //Main Class
